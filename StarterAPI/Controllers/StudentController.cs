bb23e7e95de5279b9d974344022b44fbe8775844
uwang20 <ohjuan205@gmail.com>
2022-05-10 12:02:31 +0800

    Entities and Controller

---
+++ StarterAPI/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StarterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteStudent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StarterAPI/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace StarterAPI.Entities
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string StudentNo { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+        public DateTime? DateEnrolled { get; set; }
+    }
+
+
+}
